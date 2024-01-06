11a1e5e953b10389ef15d475f9b3ddd01a1c09b7
Alexis_Lienard <lienard.alexis@gmail.com>
2024-01-06 14:11:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+// pages & components
+import Home from "./pages/Home";
+
+// styles
+import "./App.scss";
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
